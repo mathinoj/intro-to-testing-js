c9d24ef0380ed0ed6d7f56bc2275f7610e08920b
Matthias Hinojosa <hinojosamatthias@gmail.com>
2021-08-24 11:44:00 -0500

    added the third test and solution for into-to-testing

--- tests.js
+++ tests.js
@@ -21,20 +21,14 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
-    // it('should return the string "Hello, World!" when executed', function() {
-    //     expect(helloWorld()).toBe("Hello, World!");
-    // });
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
     // it("should never return 'undefined' when called", function() {
     //     expect(helloWorld()).not.toBe(undefined);
     // });
 });
 
-//Step 1: How that the function exists and returns the right data type, let's add our first realistic assertion. In tests.js, assert that sayHello("Jane") returns "Hello, Jane!". Our first test should be super simple and super small.
-// Step 2: Run all tests and make sure that this newly added test is red.
-// Step 3: If the test wants us to return "Hello, Jane!" then literally write return "Hello, Jane!"; because that's the simplest way to green a test looking for "Hello, Jane!".
-// Step 4: Run all tests. They should all be green at this point.
-// Step 5: It's too soon to refactor.
-// Step 6: Repeat step means to add another test, so let's move to the next exercise.
 
 //Open your terminal and navigate to the local directory where you cloned this project.
 // First, git status. Notice which files are tracked by git and which files have changes.
